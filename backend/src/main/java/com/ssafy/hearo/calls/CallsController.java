279c2c17c9cc2d13b3ef4bb068baca6cde243cee
HaYangT <juliago00@naver.com>
2026-01-24 17:05:36 +0900

    [feat] S14P11E106-170 S14P11E106-173 S14P11E106-431 #done: Livekit 토큰 발급 로직 구현

---
+++ backend/src/main/java/com/ssafy/hearo/calls/CallsController.java
@@ -0,0 +1,26 @@
+package com.ssafy.hearo.calls;
+
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/calls")
+@RequiredArgsConstructor
+public class CallsController {
+
+    final private CallsService callsService;
+    private static final String LIVEKIT_URL = "";
+
+    @PostMapping("/token")
+    public ResponseEntity<IssueTokenResponse> createToken(@Valid @RequestBody IssueTokenRequest request){
+        String token = callsService.createToken(request.getIdentity(),request.getRoomName());
+        return ResponseEntity.ok(new IssueTokenResponse(token,LIVEKIT_URL));
+    }
+
+}
---
+++ backend/src/main/java/com/ssafy/hearo/calls/IssueTokenResponse.java
@@ -0,0 +1,22 @@
+package com.ssafy.hearo.calls;
+
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class IssueTokenResponse {
+
+    //고객과 상담자한테 부여될 토큰
+    private String token;
+
+    //임시로 사용할 url
+    private String url;
+
+    //token으로 Response 생성
+    public static IssueTokenResponse of(String token, String url){
+        return IssueTokenResponse.builder().token(token).url(url).build();
+    }
+
+}
