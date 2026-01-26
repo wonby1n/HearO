5703cbfbf45e465b4c765ba013fb46865a6ca343
hyeyeok <juliago00@naver.com>
2026-01-26 14:41:08 +0900

    [feat] S14P11E106-369 #done 접수 정보 API 제공 구현

---
+++ backend/src/main/java/com/ssafy/hearo/domain/registration/controller/ReceiptController.java
@@ -0,0 +1,27 @@
+package com.ssafy.hearo.domain.registration.controller;
+
+
+import com.ssafy.hearo.domain.registration.dto.ReceiptResponse;
+import com.ssafy.hearo.domain.registration.service.ReceiptService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/registerations")
+public class ReceiptController {
+
+    private final Integer mockId = 123;
+    private final ReceiptService receiptService;
+
+    @GetMapping("/receipt/latest")
+    public ResponseEntity<ReceiptResponse> getLatestReceipt(HttpServletRequest request) {
+        Integer customerId = mockId;
+        return ResponseEntity.ok(receiptService.getLatestReceiptByCustomerId(customerId));
+    }
+
+}
