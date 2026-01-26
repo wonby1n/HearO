8d50b5785ed82340a8c54432f9aedaea32b23de7
조하원 <godhw1018@naver.com>
2026-01-26 15:07:01 +0900

    [feat] S14P11E106-299 S14P11E106-347 #done : 본인인증폼구현

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -25,6 +25,24 @@ const router = createRouter({
           }
         },
         {
+          path: 'consultation/info',
+          name: 'client-consultation-info',
+          component: () => import('@/views/client/ClientConsultationInfoView.vue'),
+          meta: {
+            title: '상담 신청',
+            role: 'client'
+          }
+        },
+        {
+          path: 'consultation/verification',
+          name: 'client-consultation-verification',
+          component: () => import('@/views/client/ClientConsultationVerificationView.vue'),
+          meta: {
+            title: '본인 확인',
+            role: 'client'
+          }
+        },
+        {
           path: 'call',
           name: 'client-call',
           component: () => import('@/views/client/ClientCallView.vue'),
